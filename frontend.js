fadd17b2e8eb4093106b9606e217a32c8bafd703
Vanesa Rogero <vanesarogero@gmail.com>
2022-06-30 12:22:14 +0100

    entrr

--- frontend.js
+++ frontend.js
@@ -5,7 +5,7 @@ let nombrebusqueda = [];
         verFueraDeLaFuncion = nombre;
         document.createElement.html (`p`)
         alert(verFueraDeLaFuncion);
-};
+}
 //si tenemos nombre busqueda entonces debe de hacer fetch if?
   //conectar la API
 if (nombrebusqueda=true){ //este if sobra?
@@ -14,29 +14,42 @@ let api='https://swapi.dev/api/people/'
       // hacer un for para recorrer el array  
         impresion=""
       for (let i = 0; i< datos.results.length; i++) {
-        document.createElement('p')
+        
         // crear un if dentro del for si se cumple entonces imprimir datos del json
         if (datos.results==='name') {//falta de innerhtml
-            impresion += `<h6>Descripcion de:${datos.results.name}</h6>
-            <p>Peso:${datos.results.name.height}</p>
-            <p>Color de pelo:${datos.results.name.hair_color}</p>
-            <p>Color de piel:${datos.results.name.skin_color}</p>
-            <p>Color de ojos:${datos.results.name.eye_color}</p>
-            <p>Genero:${datos.results.name.gender}</p>`
-        }  //else no tenemos ese nombre  crear el html
+            impresion += `<h6 class="impresion_title">Descripcion de:${datos.results.name}</h6>
+            <p class="impresion_p1>Peso:${datos.results.name.height}</p>
+            <p class="impresion_p2>Color de pelo:${datos.results.name.hair_color}</p>
+            <p class="impresion_p3>Color de piel:${datos.results.name.skin_color}</p>
+            <p class="impresion_p4>Color de ojos:${datos.results.name.eye_color}</p>
+            <p class="impresion_p5>Genero:${datos.results.name.gender}</p>
+            <button id="gusta" class="impresion_boton" onclick="botonFavoritos()"></button>` // crear un boton de me gusta 
+
+        } document.getElementById ("resultadobusqueda").innerHTML= impresion
+         //else no tenemos ese nombre  crear el html
         else{
            impresion= document.createElement('p')
            impresion.innerText=`<p>Lo siento pero ese nombre no lo tenemos en nuestra base de datos</p>`
-           document.body.appendChild(p)
-        }
+           
+        } document.body.appendChild(p)// como guardar en un punto especifico del dom?
     }
     } 
 }
-
- 
-  // crear un boton de me gusta 
-  // si le da a ese boton guardar datos el en lovalsorage con json.stringify           
+  
+  // favoritos         
                 
+  document.getElementById("impresion_boton").addEventListener(function botonFavoritos(e)) {
 
+    // hacemos que no se ejecute el enlace
+    e.preventDefault();
+  
+    // leemos los datos clave del producto y los guardamos en un objeto
+    // leemos los datos clave del producto y los guardamos en un objeto
+    let impresion = {
+    id: document.getElementById("producto-id").value,
+    nombre: document.getElementById("producto-nombre").textContent,
+    url: document.location.href
+  };
+      
 
-
+    }}
