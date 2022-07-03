d5d01bc714f4941864b1cbd8801dea126298ac88
Vanesa Rogero <vanesarogero@gmail.com>
2022-07-04 00:55:59 +0100

    entrar

--- clase.js
+++
@@ -1,5 +0,0 @@
-let nombre= ["Vanesa, Laura, alicia, jose, pepe"]
-
-for (let i= 0;i < nombre.length;i++) {
-    
-}
--- frontend.js
+++ frontend.js
@@ -1,37 +1,70 @@
-let nombrebusqueda = [];    
-    function busqueda(){
-                    nombre = document.getElementById("inputbusqueda").value;
-        verFueraDeLaFuncion = nombre;
-        document.createElement.html (`p`)
-        alert(verFueraDeLaFuncion);
+let personajes = [];
+
+function imprimirObjeto(personaje) {
+ let elResultadoBusqueda = document.getElementById("resultado-busqueda");
+  let impresion = "";
+
+  impresion += `<h6 class="impresion_title">Descripcion de: ${personaje.name}"</h6>" 
+      <p>Altura: ${personaje.mass} </p>
+      <p>Peso: ${personaje.height} </p>
+      <p>Color ojos: ${personaje.eye_color} </p>
+      <p>Color piel: ${personaje.skin_color} </p>
+      <p>Color pelo: ${personaje.hair_color} </p>
+      <p>Fecha nacimiento: ${personaje.birth_year} </p>
+      <p>Género: ${personaje.gender}</p>
+      <a id="boton-busqueda" class="busqueda_favoritos">Botón de favoritos</a>`
+      ;
+
+  elResultadoBusqueda.innerHTML = impresion;
 }
 
+// <button id="gusta" class="impresion_boton" onclick="botonFavoritos()"></button>`;
 
+function busqueda(){
+  //if (json.next es vacio) llamaapi(json.next) 
+  Else parseo(json.array) {
+    boton
+  }
+if (name=0){
+	fetch('https://swapi.dev/api/people/')
+	  .then(response => response.json())
+	  .then(data => recogerRespuestas(data));
 
-    fetch('https://swapi.dev/api/people/').then(response => response.json()) .then(data => imprimirobjeto(data));
-    //problema con la api hay que hacer 8 llamadas con el fetch se podria hacer con un for? o habria que hacerlo con 
+}
+}
 
+                
+
+      
+function recogerRespuestas(respuestas){
+	console.log(respuestas);
+ 	var impresion=""
+	var name = document.getElementById("inputbusqueda").value;
+
+	for (let i = 0; i< respuestas.results.length; i++) {
+		console.log(respuestas.results[i].name +" "+ respuestas.results[i].height +" "+respuestas.results[i].mass   )
+
+		if(respuestas.results[i].name ==name){
+          			  impresion += '<h6 class="impresion_title">Descripcion de:' + respuestas.results[i].name  + '</h6>'+
+          		 	 '<p class="impresion_p4">Color de ojos:'+ respuestas.results[i].height +'</p>'
+		}
+
+	}
+
+ 	document.getElementById ("resultadobusqueda").innerHTML= impresion
+}
+
+      
 function imprimirobjeto(datos){
-let impresion = '';
-console.log(datos)
-
-     for (let i = 0; i< datos.results.length; i++) {
-        console.log(datos.results[i].name)
-        // crear un if dentro del for si se cumple entonces imprimir datos del json
-        
-       
-            impresion += '<h6 class="impresion_title">Descripcion de:</h6>'+datos.results[i].name+'</h6>'+
-            '<p class="impresion_p1>Peso:'+datos.results[i].height+'</p>'+
-		//aniadir mas lineas con mas detalles
-            '<button id="gusta" class="impresion_boton" onclick="botonFavoritos()"></button>' // crear un boton de me gusta 
-	   console.log(impresion);
-
-    
-    }
-    document.getElementById ("resultadobusqueda").innerHTML= impresion;
-
+	console.log("entro en imprimirobjeto");
 }
 
-.catch(function error(err) {
-    console.error(err);
-});
+   
+function imprimirobjeto(datos){
+	console.log("entro en imprimirobjeto");
+}
+
+   
+function botonFavoritos(datos){
+	console.log("entro en boton");
+}
